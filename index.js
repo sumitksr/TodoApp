6fd09bf1d041592f72cf143a3f6d73a5eb0af32d
sumitksr <sumitksr6223@gmail.com>
2025-06-21 10:08:44 +0530

    Fix typo in dotenv usage and adjust server start log message

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // initializing express 
 const express = require('express');
 const app = express();
-
+app.use(express.json());
 // ✅ Typo fix: 'dontenv' → 'dotenv'
 require("dotenv").config();
 
 const PORT = process.env.PORT || 4000; // default to 4000 if PORT is not set
 
-app.use(express.json());
+
 const todoRoutes=require("./routes/Todos");
 
 app.use("/api/v1",todoRoutes);
+app.listen(PORT ,()=>{
+    console.log(`server started sycessfully at ${PORT}`)
+});
 
